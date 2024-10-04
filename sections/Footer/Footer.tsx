3b4a899f247c5040253d6addc2c4df6bedefbe06
decobot <capy@deco.cx>
2024-10-04 17:26:55 -0300

    fix(footer): small fixes

--- sections/Footer/Footer.tsx
+++ sections/Footer/Footer.tsx
@@ -34,9 +34,9 @@ function Footer({
         <div class="flex justify-between gap-5 md:gap-10 max-md:flex-col text-secondary">
           {collumns.map((props) => <Collumn {...props} />)}
         </div>
-        <div class="flex justify-between gap-10 lg:h-12 items-center max-lg:flex-wrap max-lg:justify-center">
+        <div class="flex justify-between gap-8 md:gap-10 lg:h-12 items-center max-lg:flex-wrap max-lg:justify-center">
           <div
-            class="text-xxs text-info max-w-[684px] text-center"
+            class="text-xxs text-info max-w-[684px]"
             dangerouslySetInnerHTML={{ __html: richText }}
           />
           <div class="flex items-center flex-shrink">
